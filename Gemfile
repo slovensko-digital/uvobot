f2bd85f20a0a96d64e2bc2ae9f1dfe1761e5e53c
Jan Suchal <jan.suchal@gmail.com>
2016-01-23 20:47:20 +0100

    Refactor a bit, closes #4

--- Gemfile
+++ Gemfile
@@ -4,4 +4,5 @@ ruby '2.2.2'
 
 gem 'nokogiri'
 gem 'curb'
+gem 'dotenv'
 
--- uvobot.rb
+++ uvobot.rb
@@ -2,40 +2,43 @@ require 'curb'
 require 'nokogiri'
 require 'json'
 require 'uri'
+require 'dotenv'
 
-url = 'https://www2.uvo.gov.sk/evestnik?p_p_id=evestnik_WAR_eVestnikPortlets&p_p_lifecycle=1&p_p_state=normal&p_p_mode=view&p_p_col_id=column-2&p_p_col_pos=1&p_p_col_count=2'
+Dotenv.load!
+
+SEARCH_URL = 'https://www2.uvo.gov.sk/evestnik?p_p_id=evestnik_WAR_eVestnikPortlets&p_p_lifecycle=1&p_p_state=normal&p_p_mode=view&p_p_col_id=column-2&p_p_col_pos=1&p_p_col_count=2'
+NEW_ISSUE_URL = 'https://www2.uvo.gov.sk/evestnik/-/vestnik/aktual'
 
 def send_message(text)
-  payload = {text: text, channel: "#general", username: "uvobot", icon_emoji: ":mag_right:"}
+  payload = { text: text, channel: '@johno', username: 'uvobot', icon_emoji: ':mag_right:' }
   Curl.post(ENV['UVOBOT_SLACK_WEBHOOK'], payload.to_json)
 end
 
 def issue_ready?
-  urlTodayCheck = 'https://www2.uvo.gov.sk/evestnik/-/vestnik/aktual'
-  identifier = Time.now.strftime('/%Y - %-d.%-m.%Y ')
-  html = Curl.get(urlTodayCheck).body
-  doc = Nokogiri::HTML.parse(html)
-  return doc.css('h1')[1].inner_html.include?(identifier)
+  html = Curl.get(NEW_ISSUE_URL).body
+  header = Nokogiri::HTML.parse(html).css('h1')[1]
+  identifier = Time.now.strftime('/%Y - %-d.%-m.%Y')
+  header.text.include?(identifier)
 end
 
-if !issue_ready?
-  send_message('Fíha, dnes vestník nevyšiel?')
+unless issue_ready?
+  send_message('*Fíha, dnes na ÚVO nevyšlo nové vydanie vestníka?*')
   exit
 end
 
 today = Time.now.strftime('%d.%m.%Y')
 vars = { cpv: 72, datumZverejneniaOd: today, datumZverejneniaDo: today}
 
-html = Curl.post(url, vars).body
+html = Curl.post(SEARCH_URL, vars).body
 doc = Nokogiri::HTML.parse(html)
 
 rows = doc.css('.oznamenie')
 
 if rows.count == 0 
-  send_message('Dnes som nenašiel žiadne nové IT zákazky na ÚVO.')
+  send_message('Dnes som nenašiel žiadne nové IT zákazky.')
 else
   counter = doc.css('.search-results').first.text
-  send_message("Dnes som našiel niečo nové na ÚVO! #{counter}")
+  send_message("Našiel niečo nové na ÚVO! #{counter}")
 end
 
 rows.each do |row|
