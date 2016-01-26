a5f78e3490a471a446034fbbdfe8d9d2ac454a11
saboter <martin.sabo@pymutan.com>
2016-01-26 08:52:05 +0100

    Constants freeze.

--- uvo_scraper.rb
+++ uvo_scraper.rb
@@ -3,8 +3,8 @@ require 'nokogiri'
 
 class UvoScraper
 
-  SEARCH_URL = 'https://www2.uvo.gov.sk/evestnik?p_p_id=evestnik_WAR_eVestnikPortlets&p_p_lifecycle=1&p_p_state=normal&p_p_mode=view&p_p_col_id=column-2&p_p_col_pos=1&p_p_col_count=2'
-  NEW_ISSUE_URL = 'https://www2.uvo.gov.sk/evestnik/-/vestnik/aktual'
+  SEARCH_URL = 'https://www2.uvo.gov.sk/evestnik?p_p_id=evestnik_WAR_eVestnikPortlets&p_p_lifecycle=1&p_p_state=normal&p_p_mode=view&p_p_col_id=column-2&p_p_col_pos=1&p_p_col_count=2'.freeze
+  NEW_ISSUE_URL = 'https://www2.uvo.gov.sk/evestnik/-/vestnik/aktual'.freeze
 
   def issue_ready?
     html = Curl.get(NEW_ISSUE_URL).body
