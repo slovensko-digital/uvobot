a9921e83f5ea01bf8dbf03623387d9e8a4a4e187
saboter <martin.sabo@pymutan.com>
2016-02-04 07:38:53 +0100

    Removed notifications module file.

--- lib/uvobot/notifications.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'notifications/notifier'
-require_relative 'notifications/slack_notifier'
-require_relative 'notifications/discourse_notifier'
-
-module Uvobot
-  module Notifications
-  end
-end
