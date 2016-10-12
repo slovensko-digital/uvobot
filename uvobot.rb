63a298975d3d036856cc7d9ce2f4c57300d520e4
Jano Suchal <jan.suchal@gmail.com>
2016-10-12 09:51:09 +0200

    Make notifiers optional

--- uvobot.rb
+++ uvobot.rb
@@ -3,20 +3,25 @@ require 'date'
 require_relative 'lib/uvobot'
 
 Dotenv.load
-discourse_client = Uvobot::DiscourseClient.new(
-  ENV.fetch('DISCOURSE_URL'),
-  ENV.fetch('DISCOURSE_API_KEY'),
-  ENV.fetch('DISCOURSE_USER')
-)
 
-notifiers = [
-  Uvobot::Notifications::SlackNotifier.new(ENV.fetch('UVOBOT_SLACK_WEBHOOK')),
-  Uvobot::Notifications::DiscourseNotifier.new(
+notifiers = []
+
+if ENV['UVOBOT_SLACK_WEBHOOK']
+  notifiers << Uvobot::Notifications::SlackNotifier.new(ENV.fetch('UVOBOT_SLACK_WEBHOOK'))
+end
+
+if ENV['DISCOURSE_URL']
+  discourse_client = Uvobot::DiscourseClient.new(
+    ENV.fetch('DISCOURSE_URL'),
+    ENV.fetch('DISCOURSE_API_KEY'),
+    ENV.fetch('DISCOURSE_USER')
+  )
+  notifiers << Uvobot::Notifications::DiscourseNotifier.new(
     discourse_client,
     ENV.fetch('DISCOURSE_TARGET_CATEGORY'),
     Uvobot::UvoScraper.new
   )
-]
+end
 
 Uvobot::Worker.new(
   Uvobot::UvoScraper.new,
